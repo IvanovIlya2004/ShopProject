468d0c4f9f2d237021cebae941d5ab04a66efae4
IvanovIlya2004 <116782889+IvanovIlya2004@users.noreply.github.com>
2022-10-28 16:28:33 +0300

    Update SellerBuyer.java

--- SellerBuyer.java
+++ SellerBuyer.java
@@ -8,19 +8,31 @@ public class SellerBuyer {
     }
 
     public void endFirstSeller() {
+        System.out.println("");
+        System.out.println("Продавец : ");
         System.out.println("Спасибо за покупку!");
     }
     public void endSecondSeller() {
+        System.out.println("");
+        System.out.println("Продавец : ");
         System.out.println("Оформляю бронь");
     }
 
     public void endFirstBuyer() {
+        System.out.println("");
+        System.out.println("Покупатель : ");
         System.out.println("Покупаю");
     }
     public void endSecondBuyer() {
+        System.out.println("");
+        System.out.println("Покупатель : ");
         System.out.println("Готов забронировать");
     }
     public void endThirdBuyer() {
+        System.out.println("");
+        System.out.println("Покупатель : ");
         System.out.println("Жаль, до свидания");
     }
+
+
 }
